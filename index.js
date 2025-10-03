6f0ceeb9101c195840d0a7b2926ffea38b09a48f
rick-kenti <erickmakenti@gmail.com>
2025-10-03 06:56:19 +0300

    title case array

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+function titleCased() {
+  const tutorials = [
+    "what does the this keyword mean?",
+    "what is the constructor OO pattern?",
+    "implementing blockchain web API",
+    "the test driven development workflow",
+    "what is NaN and how can we check for it",
+    "what is the difference between stopPropagation and preventDefault?",
+    "immutable state and pure functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?"
+  ];
 
-const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    return tutorial.replace(/\b\w/g, char => char.toUpperCase());
+  });
 }
